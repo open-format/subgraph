c4f6115cf2334d78086d0259064a3ad9c13c6b89
Andy <andy@openformat.tech>
2023-02-09 17:32:39 +0000

    Add IDTemplates helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,9 +1,13 @@
+import { NFTId } from "./idTemplates";
 import { loadOrCreateApp } from "./loadOrCreateApp";
 import { loadOrCreateContract } from "./loadOrCreateContract";
 import { loadOrCreateContractMetadata } from "./loadOrCreateContractMetadata";
+import { loadOrCreateNFT } from "./loadOrCreateNFT";
 
 export {
   loadOrCreateApp,
   loadOrCreateContract,
   loadOrCreateContractMetadata,
+  loadOrCreateNFT,
+  NFTId,
 };
