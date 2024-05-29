3bf3ada993cd7120dd87db95425b64c3eb6fa7dc
Damian I <nupipem@gmail.com>
2024-05-29 03:45:15 -0400

    Added tests for token functionalities

---
+++ tests/core/ERC20Base.test.ts
@@ -0,0 +1,184 @@
+import { assert, createMockedFunction, clearStore, test, newMockEvent, newMockCall, countEntities, mockIpfsFile, beforeAll, describe, afterEach, afterAll, mockInBlockStore, clearInBlockStore, logStore, dataSourceMock } from "matchstick-as/assembly/index"
+import { Param, ParamType, createApp, createERC20Token, getTestFungibleToken, newEvent, transferERC20, updateERC20ContractURI } from "../utils";
+import { TEST_APPSTATS_ENTITY_TYPE, TEST_APP_ENTITY_TYPE, TEST_APP_ID, TEST_APP_NAME, TEST_FUNGIBLETOKENBALANCE_ENTITY_TYPE, TEST_FUNGIBLETOKENMETADATA_ENTITY_TYPE, TEST_FUNGIBLETOKEN_ENTITY_TYPE, TEST_STATS_ENTITY_TYPE, TEST_TOKEN_ID, TEST_TOKEN_IMPLEMENTATIONID_BASE, TEST_TOKEN_NAME, TEST_TOKEN_SYMBOL, TEST_TOKEN_TOTAL_SUPPLY, TEST_USER2_ID, TEST_USER3_ID, TEST_USER_ENTITY_TYPE, TEST_USER_ID } from "../fixtures";
+import { Address, BigInt, DataSourceContext, Value, ethereum } from "@graphprotocol/graph-ts";
+import { TokenBalanceId } from "../../src/helpers";
+
+describe("ERC20Base tests", () => {
+    afterEach(() => {
+      clearStore();
+      dataSourceMock.resetValues();
+    })
+    
+    test("Token transfer", () => {
+        const sender = Address.fromString(TEST_USER2_ID);
+        const receiver = Address.fromString(TEST_USER3_ID);
+        const balanceSender = BigInt.fromString("12");
+        const balanceReceiver = BigInt.fromString("34");
+        const totalSupply = BigInt.fromString(TEST_TOKEN_TOTAL_SUPPLY);
+        let context = new DataSourceContext()
+        
+        context.set('ERC20Contract', Value.fromString(TEST_TOKEN_ID))
+        dataSourceMock.setReturnValues(TEST_TOKEN_ID, 'ERC20Contract', context)
+
+        createMockedFunction(Address.fromString(TEST_TOKEN_ID), "balanceOf", "balanceOf(address):(uint256)")
+            .withArgs([ ethereum.Value.fromAddress( sender ) ])
+            .returns([ ethereum.Value.fromUnsignedBigInt(balanceSender) ]);
+            
+        createMockedFunction(Address.fromString(TEST_TOKEN_ID), "balanceOf", "balanceOf(address):(uint256)")
+            .withArgs([ ethereum.Value.fromAddress( receiver ) ])
+            .returns([ ethereum.Value.fromUnsignedBigInt(balanceReceiver) ]);
+
+        createMockedFunction(Address.fromString(TEST_TOKEN_ID), "totalSupply", "totalSupply():(uint256)")
+            .withArgs([])
+            .returns([ ethereum.Value.fromUnsignedBigInt(totalSupply) ]);
+
+        const token = getTestFungibleToken();
+        token.save();
+
+        transferERC20();
+
+        // Users data
+        assert.fieldEquals(TEST_USER_ENTITY_TYPE, TEST_USER2_ID, "id", TEST_USER2_ID); // Sender
+        assert.fieldEquals(TEST_USER_ENTITY_TYPE, TEST_USER3_ID, "id", TEST_USER3_ID); // Receiver
+
+        // Fungible token data
+        assert.fieldEquals(TEST_FUNGIBLETOKEN_ENTITY_TYPE, TEST_TOKEN_ID, "id", TEST_TOKEN_ID);
+        assert.fieldEquals(TEST_FUNGIBLETOKEN_ENTITY_TYPE, TEST_TOKEN_ID, "totalSupply", totalSupply.toString());
+
+        const senderTokenBalanceId = TokenBalanceId(Address.fromString(TEST_TOKEN_ID), sender);
+        const receiverTokenBalanceId = TokenBalanceId(Address.fromString(TEST_TOKEN_ID), receiver);
+
+        // Sender balance
+        assert.fieldEquals(TEST_FUNGIBLETOKENBALANCE_ENTITY_TYPE, senderTokenBalanceId, "id", senderTokenBalanceId);
+        assert.fieldEquals(TEST_FUNGIBLETOKENBALANCE_ENTITY_TYPE, senderTokenBalanceId, "user", sender.toHex());
+        assert.fieldEquals(TEST_FUNGIBLETOKENBALANCE_ENTITY_TYPE, senderTokenBalanceId, "token", TEST_TOKEN_ID);
+        assert.fieldEquals(TEST_FUNGIBLETOKENBALANCE_ENTITY_TYPE, senderTokenBalanceId, "balance", balanceSender.toString());
+
+        // Receiver balance
+        assert.fieldEquals(TEST_FUNGIBLETOKENBALANCE_ENTITY_TYPE, receiverTokenBalanceId, "id", receiverTokenBalanceId);
+        assert.fieldEquals(TEST_FUNGIBLETOKENBALANCE_ENTITY_TYPE, receiverTokenBalanceId, "user", receiver.toHex());
+        assert.fieldEquals(TEST_FUNGIBLETOKENBALANCE_ENTITY_TYPE, receiverTokenBalanceId, "token", TEST_TOKEN_ID);
+        assert.fieldEquals(TEST_FUNGIBLETOKENBALANCE_ENTITY_TYPE, receiverTokenBalanceId, "balance", balanceReceiver.toString());
+    })
+
+    test("Token transfer minted", () => {
+        const sender = Address.fromString("0x0000000000000000000000000000000000000000");
+        const receiver = Address.fromString(TEST_USER3_ID);
+        const balanceSender = BigInt.fromString("0");
+        const balanceReceiver = BigInt.fromString("34");
+        const totalSupply = BigInt.fromString(TEST_TOKEN_TOTAL_SUPPLY);
+        let context = new DataSourceContext()
+        
+        context.set('ERC20Contract', Value.fromString(TEST_TOKEN_ID))
+        dataSourceMock.setReturnValues(TEST_TOKEN_ID, 'ERC20Contract', context)
+
+        createMockedFunction(Address.fromString(TEST_TOKEN_ID), "balanceOf", "balanceOf(address):(uint256)")
+            .withArgs([ ethereum.Value.fromAddress( sender ) ])
+            .returns([ ethereum.Value.fromUnsignedBigInt(balanceSender) ]);
+            
+        createMockedFunction(Address.fromString(TEST_TOKEN_ID), "balanceOf", "balanceOf(address):(uint256)")
+            .withArgs([ ethereum.Value.fromAddress( receiver ) ])
+            .returns([ ethereum.Value.fromUnsignedBigInt(balanceReceiver) ]);
+
+        createMockedFunction(Address.fromString(TEST_TOKEN_ID), "totalSupply", "totalSupply():(uint256)")
+            .withArgs([])
+            .returns([ ethereum.Value.fromUnsignedBigInt(totalSupply) ]);
+
+        const token = getTestFungibleToken();
+        token.save();
+
+        transferERC20();
+
+        // Users data
+        assert.fieldEquals(TEST_USER_ENTITY_TYPE, TEST_USER3_ID, "id", TEST_USER3_ID); // Receiver
+        assert.notInStore(TEST_USER_ENTITY_TYPE, sender.toHex());
+
+        // Fungible token data
+        assert.fieldEquals(TEST_FUNGIBLETOKEN_ENTITY_TYPE, TEST_TOKEN_ID, "id", TEST_TOKEN_ID);
+        assert.fieldEquals(TEST_FUNGIBLETOKEN_ENTITY_TYPE, TEST_TOKEN_ID, "totalSupply", totalSupply.toString());
+
+        const senderTokenBalanceId = TokenBalanceId(Address.fromString(TEST_TOKEN_ID), sender);
+        const receiverTokenBalanceId = TokenBalanceId(Address.fromString(TEST_TOKEN_ID), receiver);
+
+        // Sender
+        assert.notInStore(TEST_FUNGIBLETOKENBALANCE_ENTITY_TYPE, senderTokenBalanceId);
+
+        // Receiver balance
+        assert.fieldEquals(TEST_FUNGIBLETOKENBALANCE_ENTITY_TYPE, receiverTokenBalanceId, "id", receiverTokenBalanceId);
+        assert.fieldEquals(TEST_FUNGIBLETOKENBALANCE_ENTITY_TYPE, receiverTokenBalanceId, "user", receiver.toHex());
+        assert.fieldEquals(TEST_FUNGIBLETOKENBALANCE_ENTITY_TYPE, receiverTokenBalanceId, "token", TEST_TOKEN_ID);
+        assert.fieldEquals(TEST_FUNGIBLETOKENBALANCE_ENTITY_TYPE, receiverTokenBalanceId, "balance", balanceReceiver.toString());
+    })
+
+    test("Token transfer burned", () => {
+        const sender = Address.fromString(TEST_USER2_ID);
+        const receiver = Address.fromString("0x0000000000000000000000000000000000000000");
+        const balanceSender = BigInt.fromString("12");
+        const balanceReceiver = BigInt.fromString("0");
+        const totalSupply = BigInt.fromString(TEST_TOKEN_TOTAL_SUPPLY);
+        let context = new DataSourceContext()
+        
+        context.set('ERC20Contract', Value.fromString(TEST_TOKEN_ID))
+        dataSourceMock.setReturnValues(TEST_TOKEN_ID, 'ERC20Contract', context)
+
+        createMockedFunction(Address.fromString(TEST_TOKEN_ID), "balanceOf", "balanceOf(address):(uint256)")
+            .withArgs([ ethereum.Value.fromAddress( sender ) ])
+            .returns([ ethereum.Value.fromUnsignedBigInt(balanceSender) ]);
+            
+        createMockedFunction(Address.fromString(TEST_TOKEN_ID), "balanceOf", "balanceOf(address):(uint256)")
+            .withArgs([ ethereum.Value.fromAddress( receiver ) ])
+            .returns([ ethereum.Value.fromUnsignedBigInt(balanceReceiver) ]);
+
+        createMockedFunction(Address.fromString(TEST_TOKEN_ID), "totalSupply", "totalSupply():(uint256)")
+            .withArgs([])
+            .returns([ ethereum.Value.fromUnsignedBigInt(totalSupply) ]);
+
+        const token = getTestFungibleToken();
+        token.save();
+
+        const event = transferERC20();
+
+        // Users data
+        assert.fieldEquals(TEST_USER_ENTITY_TYPE, TEST_USER2_ID, "id", TEST_USER2_ID); // Sender
+        assert.notInStore(TEST_USER_ENTITY_TYPE, receiver.toHex()); // Receiver
+
+        // Fungible token data
+        assert.fieldEquals(TEST_FUNGIBLETOKEN_ENTITY_TYPE, TEST_TOKEN_ID, "id", TEST_TOKEN_ID);
+        assert.fieldEquals(TEST_FUNGIBLETOKEN_ENTITY_TYPE, TEST_TOKEN_ID, "totalSupply", totalSupply.toString());
+        // TODO: burntSupply does not increase
+        // assert.fieldEquals(TEST_FUNGIBLETOKEN_ENTITY_TYPE, TEST_TOKEN_ID, "burntSupply", event.params.value.toString());
+
+        const senderTokenBalanceId = TokenBalanceId(Address.fromString(TEST_TOKEN_ID), sender);
+        const receiverTokenBalanceId = TokenBalanceId(Address.fromString(TEST_TOKEN_ID), receiver);
+
+        // Sender balance
+        assert.fieldEquals(TEST_FUNGIBLETOKENBALANCE_ENTITY_TYPE, senderTokenBalanceId, "id", senderTokenBalanceId);
+        assert.fieldEquals(TEST_FUNGIBLETOKENBALANCE_ENTITY_TYPE, senderTokenBalanceId, "user", sender.toHex());
+        assert.fieldEquals(TEST_FUNGIBLETOKENBALANCE_ENTITY_TYPE, senderTokenBalanceId, "token", TEST_TOKEN_ID);
+        assert.fieldEquals(TEST_FUNGIBLETOKENBALANCE_ENTITY_TYPE, senderTokenBalanceId, "balance", balanceSender.toString());
+
+        // Receiver balance
+        assert.notInStore(TEST_FUNGIBLETOKENBALANCE_ENTITY_TYPE, receiverTokenBalanceId);
+    })
+
+    test("Contract URI updated", () => {
+        let context = new DataSourceContext()
+        context.set('ERC20Contract', Value.fromString(TEST_TOKEN_ID))
+        dataSourceMock.setReturnValues(TEST_TOKEN_ID, 'ERC20Contract', context)
+
+        const token = getTestFungibleToken();
+        token.save();
+
+        const event = updateERC20ContractURI();
+
+        // Fungible token data
+        assert.fieldEquals(TEST_FUNGIBLETOKEN_ENTITY_TYPE, TEST_TOKEN_ID, "id", TEST_TOKEN_ID);
+        assert.fieldEquals(TEST_FUNGIBLETOKEN_ENTITY_TYPE, TEST_TOKEN_ID, "metadata", TEST_TOKEN_ID);
+
+        // Metadata
+        assert.fieldEquals(TEST_FUNGIBLETOKENMETADATA_ENTITY_TYPE, TEST_TOKEN_ID, "id", TEST_TOKEN_ID);
+        assert.fieldEquals(TEST_FUNGIBLETOKENMETADATA_ENTITY_TYPE, TEST_TOKEN_ID, "URI", event.params.newURI);
+    })
+
+});
